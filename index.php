a0336ec71299f4f07a2157236cdce60422071c20
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2023-04-19 09:58:25 +0800

    [ADD] design of the contents

--- index.php
+++ index.php
@@ -42,31 +42,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parameter Values</title>
-    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h2>Part 1</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        A cumque sint corporis id! Maiores autem, 
-        sint repellendus earum laudantium sapiente officiis voluptatum aperiam ipsam 
-        corporis iste ea dolorum blanditiis magnam. </p>
-    <a href="https://www.w3schools.com/">W3 schools</a> 
+    <table>
+        <tbody>
+            <tr>
+                <td>
+                    <h1>An Example page</h1>
+                </td>
+            </tr>
 
+            <tr>
+                <td>
+                    <p><a href="https://www.w3schools.com/">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a> A cumque sint corporis id! Maiores autem, sint repellendus earum laudantium sapiente officiis voluptatum aperiam ipsam corporis iste ea dolorum blanditiis magnam.</p> 
+                </td>
 
-    <h2>Part 2</h2>
-    <?php 
-        for ($x = 0; $x < count($words); $x++){
-            echo $words[$x]." ";
-        }
-    ?>
+                <td>
+                    <div class="btn">
+                        <button class="btn1"> Click Me </button>
+                    </div>
+                </td>
+            </tr>
 
+            <tr>
+                <td>
+                    <p>
+                        <?php 
+                        for ($x = 0; $x < count($words); $x++){
+                            echo $words[$x]." ";
+                        }
+                        ?>
+                    </p>
+                </td>
+            </tr>
 
-    <h2>Part 3</h2>
-    <p>My Parameter Values: 25, 13, 6</p>
-    <p> Addition: <?php echo addition(25, 13, 6)?></p>
-    <p> Average: <?php echo average(25, 13, 6)?></p>
-    <p> Min value: <?php echo minValue(25, 13, 6)?></p>
-    <p> Max value: <?php echo maxValue(25, 13, 6)?></p>
-
+            <tr>
+                <td>
+                    <p>My Parameter Values: 25, 13, 6</p>
+                    <p> Addition: <?php echo addition(25, 13, 6)?></p>
+                    <p> Average: <?php echo average(25, 13, 6)?></p>
+                    <p> Min value: <?php echo minValue(25, 13, 6)?></p>
+                    <p> Max value: <?php echo maxValue(25, 13, 6)?></p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
